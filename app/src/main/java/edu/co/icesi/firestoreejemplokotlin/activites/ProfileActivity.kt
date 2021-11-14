8158d1ef828fb0c6822f9c4466a7d6788745e720
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-11-14 14:23:57 -0500

    Video 3

--- app/src/main/java/edu/co/icesi/firestoreejemplokotlin/activites/ProfileActivity.kt
+++ app/src/main/java/edu/co/icesi/firestoreejemplokotlin/activites/ProfileActivity.kt
@@ -1,27 +1,84 @@
 package edu.co.icesi.firestoreejemplokotlin.activites
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AlertDialog
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.google.gson.Gson
 import edu.co.icesi.firestoreejemplokotlin.R
 import edu.co.icesi.firestoreejemplokotlin.databinding.ActivityProfileBinding
+import edu.co.icesi.firestoreejemplokotlin.models.User
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityProfileBinding
+
+    private lateinit var galleryLauncher : ActivityResultLauncher<Intent>
+
+    private lateinit var user:User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        user = loadUser()!!
 
-        binding.changepassBtn.setOnClickListener(::changePassword);
+        getUpdatedUser()
+
+
+        galleryLauncher = registerForActivityResult(StartActivityForResult(), ::onGalleryResult)
+
+        binding.changepassBtn.setOnClickListener(::changePassword)
+
+        binding.profileImg.setOnLongClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            galleryLauncher.launch(intent)
+            true
+        }
+
+    }
+
+    fun getUpdatedUser(){
+        Firebase.firestore.collection("users").document(user.id).get().addOnSuccessListener {
+            val updatedUser = it.toObject(User::class.java)
+            val photoID = updatedUser?.photoID
+            downloadImage(photoID)
+        }
+    }
+
+    fun downloadImage(photoID:String?){
+        Firebase.storage.getReference().child("profile").child(photoID!!).downloadUrl.addOnSuccessListener {
+            Glide.with(binding.profileImg).load(it).into(binding.profileImg)
+        }
+    }
+
+    fun onGalleryResult(result : ActivityResult){
+        if(result.resultCode == RESULT_OK){
+            val uri = result.data?.data
+            binding.profileImg.setImageURI(uri)
+
+
+            //Upload
+            val filename = UUID.randomUUID().toString()
+            Firebase.storage.getReference().child("profile").child(filename).putFile(uri!!)
+            Firebase.firestore.collection("users").document(user.id).update("photoID", filename)
+
+        }
     }
 
     fun changePassword(view: View){
@@ -55,4 +112,16 @@ class ProfileActivity : AppCompatActivity() {
         builder.show()
 
     }
+
+
+    fun loadUser():User?{
+        val sp = getSharedPreferences("appmoviles", MODE_PRIVATE)
+        val json = sp.getString("user", "NO_USER")
+        if(json == "NO_USER"){
+            return null
+        }else{
+            return Gson().fromJson(json, User::class.java)
+        }
+    }
+
 }
--- app/src/main/java/edu/co/icesi/firestoreejemplokotlin/models/User.kt
+++ app/src/main/java/edu/co/icesi/firestoreejemplokotlin/models/User.kt
@@ -6,6 +6,7 @@ data class User(
     var id:String = "",
     var name:String = "",
     var email:String = "",
+    var photoID:String? = null
 ) : Serializable{
     override fun toString(): String {
         return name
